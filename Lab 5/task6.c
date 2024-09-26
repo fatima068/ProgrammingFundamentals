a4924282095cd6160e27048937817044bdb213e3
fatima068 <113721501+fatima068@users.noreply.github.com>
2024-09-26 14:42:01 +0500

    Add files via upload

---
+++ Lab 5/task6.c
@@ -0,0 +1,7 @@
+/*TASK 6: 
+the output is 30 because the expression is evaluated in the following order:
+x = 5 > 8? 10: 1!= 2 < 5? 20 : 30
+5 is not greater than 8, so  1!= 2 < 5? 20 : 30 is evaluated
+ 2 is less than 5, so expression becomes  1!= 1? 20 : 30
+as the statement 1 != 1 is false, x is assigned the value 30
+therefore the output is 30*/
