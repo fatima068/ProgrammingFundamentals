c387128d4a1380768a42cf184b9f51c639918e03
fatima068 <113721501+fatima068@users.noreply.github.com>
2024-09-27 21:14:04 +0500

    Update task3.c

--- Lab 5/task3.c
+++ Lab 5/task3.c
@@ -10,4 +10,50 @@ int main () {
 	scanf(" %c", &doublecup);
 	printf ("is coffee manual? enter y for yes, else enter n ");
 	scanf(" %c", &manual);
+	
+	switch(coffeetype)
+        {
+        case 'w':
+        case 'W':
+                printf("Step 1: Put Water (15 mins)\n");
+                time = time + 15;
+                printf("Step 2: Add Sugar (15 mins)\n");
+                time = time + 15;
+                if (manual == 'y' || manual == 'Y')
+                        { printf("Step 3: Mix Well (20 mins)\n");
+                          time = time + 20; }                
+                printf("Step 4: Add Coffee (2 mins)\n");
+                time = time + 2;
+                printf("Step 5: Add Milk (4 mins)\n");
+                time = time + 4;
+                if (manual == 'y' || manual == 'Y')
+                        { printf("Step 6: Mix Well (20 mins)\n");
+                          time = time + 20; }        
+        break;
+        case 'b':
+        case 'B':
+                printf("Step 1: Put Water (20 mins)\n");
+                time = time + 20;
+                printf("Step 2: Add Sugar (20 mins)\n");
+                time = time + 20;
+                if (manual == 'y' || manual == 'Y')
+                        { printf("Step 3: Mix Well (25 mins)\n");
+                          time = time + 25; }                
+                printf("Step 4: Add Coffee (15 mins)\n");
+                time = time + 15;
+                if (manual == 'y' || manual == 'Y')
+                        { printf("Step 5: Mix Well (25 mins)\n");
+                          time = time + 25; }        
+        break;
+
+        default:
+        printf("Invalid coffee type!");
+        }
+
+        if (doublecup == 'y' || doublecup == 'Y')
+                { time = time + (time / 2); }
+
+        printf("Total time taken: %d minutes", time);        
+    
+
 }
