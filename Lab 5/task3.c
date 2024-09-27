5170437071125c10aad230f5ae09f11e9372c642
fatima068 <113721501+fatima068@users.noreply.github.com>
2024-09-27 20:30:19 +0500

    Update task3.c

--- Lab 5/task3.c
+++ Lab 5/task3.c
@@ -1,4 +1,10 @@
 #include <stdio.h>
 int main () {
-	printf("");
+	char coffeetype, doublecup, manual;
+	printf("enter b for black and w for white coffee ");
+	scanf(" %c", &coffeetype);
+	printf ("enter y if you want double, else enter n ");
+	scanf(" %c", &doublecup);
+	printf ("is coffee manual? enter y for yes, else enter n ");
+	scanf(" %c", &manual);
 }
