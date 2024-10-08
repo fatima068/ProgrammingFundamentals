f98a449fcd80db38559ffaadb71400e0c395b156
fatima068 <113721501+fatima068@users.noreply.github.com>
2024-10-08 20:06:32 +0500

    Update task8 alt.c

--- Lab 6/task8 alt.c
+++ Lab 6/task8 alt.c
@@ -8,22 +8,21 @@
 */
 
 #include <stdio.h>
-int main()
-	{
+int main(){
 	int i;
 	for (i=0; i<5; i++)
 	{
-		if (i == 0 || i == 4)
-		{
-	  	printf("* * * * *\n");
-		}
-    else if (i == 1 || i == 3)
-   {
-			printf("  *   *  \n");
-		}
-    else
-    {
-			printf("    *    \n");
-		 }
+	if (i == 0 || i == 4)
+	{
+	printf("* * * * *\n")
+	}
+  else if (i == 1 || i == 3)
+  {
+	printf("  *   *  \n");
+	}
+  else
+  {
+	printf("    *    \n");
+  }
 	}
 }
