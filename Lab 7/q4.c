11cd06f8c8cab1c38fad7e3af2afe12d84430e52
fatima068 <113721501+fatima068@users.noreply.github.com>
2024-10-16 08:01:14 +0500

    Update Lab 7

---
+++ Lab 7/q4.c
@@ -0,0 +1,51 @@
+//Write a program which identifies if a given string is a palindrome using arrays.
+
+#include <stdio.h>
+int main ()
+{
+    char userIn[50], str[50];
+    printf ("enter a short string\n");
+    scanf("%s", str);
+    char *endptr = &str[0], *startptr = &str[0];
+    
+    int length=0, i=0;
+    while (str[i] != '\0')
+    {
+        length ++;
+        endptr++;
+        i++;
+    }
+    printf("length of string is %d\n", length);
+    
+    //storing original string in a new variable
+    for(i=0; i<length; i++)
+    {
+        userIn[i] = str[i];
+    }
+    
+    
+    //reversing the original string 
+    char temp;  
+    endptr--;
+    while(startptr < endptr)
+    {
+        temp = *startptr;
+        *startptr = *endptr;
+        *endptr = temp;
+        startptr++;
+        endptr--;
+    }
+    printf("reversed string: %s\n", str);
+    
+    //comparing original and reversed string 
+    int flag =1;
+    for(i=0; i<length; i++)
+    {
+        if(userIn[i] != str[i])
+            flag = 0;
+    }
+    if (flag == 0)
+        printf ("not palindrome");
+    else 
+        printf ("palindrome");
+}
