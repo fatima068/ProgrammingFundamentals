7ce87c69ba7236bb49921493b04346d862b78f8b
fatima068 <113721501+fatima068@users.noreply.github.com>
2024-11-06 19:06:22 +0500

    Update Lab 9

---
+++ Lab 9/q2.c
@@ -0,0 +1,23 @@
+/*Create a C program that swaps the values of two integers using a user-defined function,
+swapIntegers. The user inputs two integer values, and the program uses the function to swapthem. It should perform the swap and display the updated values.*/
+
+#include <stdio.h>
+void swapIntegers(int a, int b);
+
+int main()
+{
+    int a, b;
+    printf("enter two numbers to swap\n");
+    scanf("%d", &a);
+    scanf("%d", &b);
+    printf("\nBEFORE SWAPPING\na=%d\nb=%d\n\nAFTER SWAPPING\n", a, b);
+    swapIntegers(a, b);
+}
+
+void swapIntegers(int a, int b)
+{
+    int temp = b;
+    b = a;
+    a = temp;
+    printf("a=%d\nb=%d", a, b);
+}
