e776f7ea59a9ebf24d5f17558f416bc0f16c46db
fatima068 <113721501+fatima068@users.noreply.github.com>
2024-12-02 00:42:18 +0500

    Update

---
+++ Lab 12/q1.c
@@ -0,0 +1,20 @@
+/*Task 1: Write a program that calculates the sum of all the elements in array using pointers (Note: Generate the array via user)*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n, sum=0;
+    printf ("Enter num of array elements: ");
+    scanf("%d", &n);
+    int* arr = (int*)malloc(n*sizeof(int));
+    for(int i = 0; i<5; i++)
+    {
+        printf ("Enter element %d: ", i+1);
+        scanf("%d", &arr[i]);
+        sum = sum + arr[i];
+    }
+    printf ("Sum: %d", sum);
+    free(arr);
+}
