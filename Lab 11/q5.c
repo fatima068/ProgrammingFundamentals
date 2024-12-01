e93928228a8ce63a92c34bda9316b085c621706a
fatima068 <113721501+fatima068@users.noreply.github.com>
2024-12-01 18:26:00 +0500

    Update Lab 11

---
+++ Lab 11/q5.c
@@ -0,0 +1,32 @@
+/*Define a structure Date with fields day, month, and year. Create another structure Event
+with fields event_name, date (nested Date structure), and location. Write a program to
+display all event details in a readable format.*/
+
+#include <stdio.h>
+typedef struct {
+    int day;
+    int month;
+    int year;
+}Date;
+
+typedef struct {
+    char eventname[50];
+    Date date;
+    char location[50];
+}Event;
+
+int main()
+{
+        Event event;
+        printf("Event Name: ");
+        scanf("%s", event.eventname);
+        printf("Event Location: ");
+        scanf("%s", event.location);
+        printf("Event day: ");
+        scanf("%d", &event.date.day);
+        printf("Event month: ");
+        scanf("%d", &event.date.month);
+        printf("Event year: ");
+        scanf("%d", &event.date.year);
+        printf("\nEvent Details\nEvent Name: %s\nEvent Location: %s\nDate: %d/%d/%d", event.eventname, event.location, event.date.day, event.date.month, event.date.year);
+}
